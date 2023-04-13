09a70f84de8f9776343760251eb96a276ab4dd31
Koi-AS <lightningblade.901@gmail.com>
2023-04-12 22:27:48 -0400

    Successfully added date display at the top of the page. Added some JS for the saving functionality

--- script.js
+++ script.js
@@ -8,16 +8,36 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  var saveBtn = $('.saveBtn');
+  var inputArea = $('#textInput');
+  
+  saveBtn.parent(this).on('click', updateInputArea)
+    //fil out function
+    inputArea.text(localStorage.getItem('content'));
+
+    function updateInputArea() {
+      localStorage.setItem('content', inputArea.val())
+    }
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  var currentHour = dayjs().format('H');
+
+  //add code to set time to past present or future
+  //determine if time is in the past present or the future. if else statement?
+  //set the block element to past present or future
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+
+
   // TODO: Add code to display the current date in the header of the page.
+  var dateEl = $('#currentDay')
+  var today = dayjs();
+
+  dateEl.text(today.format('dddd, MMMM D'));
 });
