f47d14be916ecd6e81ffaeac1c22a9bc5abc9ca4
Koi-AS <lightningblade.901@gmail.com>
2023-04-13 16:26:18 -0400

    I got the past, present, future display feature working. Now, when the hour changes, the timeblock display will change to gray for the past, red for the present, and green for the future

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// WELCOME TRAVELER TO MY JAVASCRIPT
+// It's a little long, but there are 4 key areas:
+//  1: Save textarea data
+//  2: Get textarea data
+//  3: Track hour of the day
+//  4: Display the current day
 
+// $(document).ready(function () {}) waits for the HTML to load before calling the function
 $(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // Saves the content of the time block to local storage // * * *   * -   * * * -   * //
   var saveBtn = $('.saveBtn');
   var hour9Input = $('#hour-9 .description');
   var hour10Input = $('#hour-10 .description');
@@ -33,6 +32,7 @@ $(document).ready(function () {
     console.log(localStorage)
   }
 
+  // Gets the content users saved into the timeblock // - - *   *   - //
   saveBtn.on('click', updateInputArea)
   hour9Input.text(localStorage.getItem('hour9content'));
   hour10Input.text(localStorage.getItem('hour10content'));
@@ -49,16 +49,146 @@ $(document).ready(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  var currentHour = dayjs().format('H');
+  var currentHour = dayjs().format('HH');
+  var hour9 = $('#hour-9');
+  var hour10 = $('#hour-10');
+  var hour11 = $('#hour-11');
+  var hour12 = $('#hour-12');
+  var hour13 = $('#hour-13');
+  var hour14 = $('#hour-14');
+  var hour15 = $('#hour-15');
+  var hour16 = $('#hour-16');
+  var hour17 = $('#hour-17');
 
-  //add code to set time to past present or future
-  //determine if time is in the past present or the future. if else statement?
-  //set the block element to past present or future
+  function timeBlockDisplay() {
+    if(currentHour == 9) {
+      hour9.removeClass('past')
+      hour9.removeClass('future')
+      hour9.addClass('present')
+    } else if(currentHour > 9) {
+      hour9.removeClass('present')
+      hour9.removeClass('future')
+      hour9.addClass('past')
+    } else if(currentHour < 9) {
+      hour9.removeClass('past')
+      hour9.removeClass('present')
+      hour9.addClass('future')
+    }
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+    if(currentHour == 10) {
+      hour10.removeClass('past')
+      hour10.removeClass('future')
+      hour10.addClass('present')
+    } else if(currentHour > 10) {
+      hour10.removeClass('present')
+      hour10.removeClass('future')
+      hour10.addClass('past')
+    } else if(currentHour < 10) {
+      hour10.removeClass('past')
+      hour10.removeClass('present')
+      hour10.addClass('future')
+    }
 
+    if(currentHour == 11) {
+      hour11.removeClass('past')
+      hour11.removeClass('future')
+      hour11.addClass('present')
+    } else if(currentHour > 11) {
+      hour11.removeClass('present')
+      hour11.removeClass('future')
+      hour11.addClass('past')
+    } else if(currentHour < 11) {
+      hour11.removeClass('past')
+      hour11.removeClass('present')
+      hour11.addClass('future')
+    }
+
+    if(currentHour == 12) {
+      hour12.removeClass('past')
+      hour12.removeClass('future')
+      hour12.addClass('present')
+    } else if(currentHour > 12) {
+      hour12.removeClass('present')
+      hour12.removeClass('future')
+      hour12.addClass('past')
+    } else if(currentHour < 12) {
+      hour12.removeClass('past')
+      hour12.removeClass('present')
+      hour12.addClass('future')
+    }
+
+    if(currentHour == 13) {
+      hour13.removeClass('past')
+      hour13.removeClass('future')
+      hour13.addClass('present')
+    } else if(currentHour > 13) {
+      hour13.removeClass('present')
+      hour13.removeClass('future')
+      hour13.addClass('past')
+    } else if(currentHour < 13) {
+      hour13.removeClass('past')
+      hour13.removeClass('present')
+      hour13.addClass('future')
+    }
+
+    if(currentHour == 14) {
+      hour14.removeClass('past')
+      hour14.removeClass('future')
+      hour14.addClass('present')
+    } else if(currentHour > 14) {
+      hour14.removeClass('present')
+      hour14.removeClass('future')
+      hour14.addClass('past')
+    } else if(currentHour < 14) {
+      hour14.removeClass('past')
+      hour14.removeClass('present')
+      hour14.addClass('future')
+    }
+
+    if(currentHour == 15) {
+      hour15.removeClass('past')
+      hour15.removeClass('future')
+      hour15.addClass('present')
+    } else if(currentHour > 15) {
+      hour15.removeClass('present')
+      hour15.removeClass('future')
+      hour15.addClass('past')
+    } else if(currentHour < 15) {
+      hour15.removeClass('past')
+      hour15.removeClass('present')
+      hour15.addClass('future')
+    }
+
+    if(currentHour == 16) {
+      hour16.removeClass('past')
+      hour16.removeClass('future')
+      hour16.addClass('present')
+    } else if(currentHour > 16) {
+      hour16.removeClass('present')
+      hour16.removeClass('future')
+      hour16.addClass('past')
+    } else if(currentHour < 16) {
+      hour16.removeClass('past')
+      hour16.removeClass('present')
+      hour16.addClass('future')
+    }
+
+    if(currentHour == 17) {
+      hour17.removeClass('past')
+      hour17.removeClass('future')
+      hour17.addClass('present')
+    } else if(currentHour > 17) {
+      hour17.removeClass('present')
+      hour17.removeClass('future')
+      hour17.addClass('past')
+    } else if(currentHour < 17) {
+      hour17.removeClass('past')
+      hour17.removeClass('present')
+      hour17.addClass('future')
+    }
+  }
+  console.log(currentHour)
+  timeBlockDisplay()
 
   // Displays the current date onto the webpage // - * *   * -   -   * //
   var dateEl = $('#currentDay')
